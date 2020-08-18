5e334f80f1ad030bfa937706dc3a2bf5f8661125
sarah-mica <smica9@gmail.com>
2020-08-18 00:14:15 -0700

    almost finished with the card view recyclerview item

--- app/src/main/java/com/android/sarahmica/bamboo/MaskedCardView.kt
+++ app/src/main/java/com/android/sarahmica/bamboo/MaskedCardView.kt
@@ -1,33 +1,37 @@
 package com.android.sarahmica.bamboo
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Path
-import android.graphics.RectF
 import android.util.AttributeSet
+import com.google.android.material.R
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.shape.ShapeAppearanceModel
 import com.google.android.material.shape.ShapeAppearancePathProvider
+import android.annotation.SuppressLint
+import android.graphics.Path
+import android.graphics.RectF
 
 /**
  * A Card view that clips the content of any shape, this should be done upstream in card,
  * working around it for now.
  */
+// TODO: personalize this more
 class MaskedCardView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = R.attr.materialCardViewStyle
 ) : MaterialCardView(context, attrs, defStyle) {
+
     @SuppressLint("RestrictedApi")
     private val pathProvider = ShapeAppearancePathProvider()
     private val path: Path = Path()
-    private val shapeAppearance: ShapeAppearanceModel = ShapeAppearanceModel(
+    private val shapeAppearance: ShapeAppearanceModel = ShapeAppearanceModel.builder(
         context,
         attrs,
         defStyle,
         R.style.Widget_MaterialComponents_CardView
-    )
+    ).build()
+
     private val rectF = RectF(0f, 0f, 0f, 0f)
 
     override fun onDraw(canvas: Canvas) {
